aef83d95b989abf8e21898df32a0f337781dbc59
Rocky.C <cloki0610@gmail.com>
2022-05-13 22:20:28 +0000

    Add hero component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,3 +1,21 @@
-const Hero = () => {};
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+const Hero = () => {
+    return (
+        <section className={classes.hero}>
+            <div className={classes.image}>
+                <Image src="/images/site/lalafell_icon3.png"
+                    alt="An image of my icon"
+                    width={300}
+                    height={300}
+                />
+            </div>
+            <h1>Hi, I'm Rocky</h1>
+            <p>I blog about web development - especially frontend framworks and API.</p>
+        </section>
+    );
+};
 
 export default Hero;
