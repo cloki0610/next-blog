582edbf77b9a13bd4970dd2294501cbe5c4591fe
Rocky.C <cloki0610@gmail.com>
2022-05-14 01:59:33 +0000

    Add all-posts component

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,31 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POST = [
+    {
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS",
+        date: "2015-02-29",
+        slug: "getting-started-with-nextjs"
+    },
+    {
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS",
+        date: "2015-02-29",
+        slug: "getting-started-with-nextjs"
+    },
+    {
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS",
+        date: "2015-02-29",
+        slug: "getting-started-with-nextjs"
+    },
+];
+
 const AllPostsPage = () => {
-    return <h1>All Posts</h1>
+    return <AllPosts posts={DUMMY_POST} />
 };
 
 export default AllPostsPage;
