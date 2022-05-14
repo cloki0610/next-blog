660beae35b73d70d3426223150577857b8af2e4e
Rocky.C <cloki0610@gmail.com>
2022-05-14 13:26:31 +0000

    Add custom renderer to replace image element

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 
 import PostHeader from "./post-header";
@@ -6,11 +7,34 @@ import classes from "./post-content.module.css";
 const PostContent = (props) => {
     const { post } = props;
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
+    const customRenderers = {
+        p(paragraph) {
+            const { node } = paragraph;
+
+            // If p element first child is image, then replace the DOM element
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`/images/posts/${post.slug}/${image.properties.src}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                );
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+    };
 
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
         </article>
     );
 };
