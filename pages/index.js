fa5f8e34592c0a81bed6bb5a2584c219377ebea0
Rocky.C <cloki0610@gmail.com>
2022-05-14 17:16:02 +0000

    Add meta data and title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -5,6 +7,10 @@ import { getFeaturedPosts } from "../lib/posts-util";
 const HomePage = (props) => {
     return (
         <>
+            <Head>
+                <title>Rocky's Blog</title>
+                <meta name="description" content="I create content about programming and web development." />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts} />
         </>
