9d015f1344fd5e6946502294487c4f34e0debeec
Rocky.C <cloki0610@gmail.com>
2022-05-14 02:37:08 +0000

    Add ReactMarkdown component

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
@@ -15,7 +17,7 @@ const PostContent = () => {
     return (
         <article className={classes.content}>
             <PostHeader title={DUMMY_POST.title} image={imagePath} />
-            Content
+            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
         </article>
     );
 };
