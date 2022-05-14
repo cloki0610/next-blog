18310fede3b32d749aa8037344979e69dd6a9cdc
Rocky.C <cloki0610@gmail.com>
2022-05-14 12:53:49 +0000

    Add getStaticPaths & getStaticProps to post detail

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,21 +3,14 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    date: "2015-02-29",
-    slug: "getting-started-with-nextjs",
-    content: "# This is the first post"
-};
-
-const PostContent = () => {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = (props) => {
+    const { post } = props;
+    const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POST.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,28 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "../../lib/posts-util";
 
-const PostDetailPage = () => {
-    return <PostContent />
+const PostDetailPage = (props) => {
+    return <PostContent post={props.post} />
 };
 
 export default PostDetailPage;
+
+export const getStaticPaths = () => {
+    const postFileNames = getPostFiles();
+    const slugs = postFileNames.map(fileName => fileName.replace(/\.md$/, ''));
+    return {
+        paths: slugs.map(slug => ({ params: { slug: slug } })),
+        fallback: 'blocking',
+    };
+};
+
+export const getStaticProps = (context) => {
+    const { params } = context;
+    const postData = getPostData(params.slug);
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600,
+    };
+};
